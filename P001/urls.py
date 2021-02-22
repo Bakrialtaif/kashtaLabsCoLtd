9a16c60b7ec93ec5246d260c27928e017d1813ab
Abobaker <altaif.bakri@gmail.com>
2021-02-22 21:39:47 +0200

    more details

--- P001/urls.py
+++ P001/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from P001.views import home
+from P001.views import home, kashta, bladalnoor, batinjan
 
 app_name = 'P001'
 
 urlpatterns = [
     path('', home.show, name='home'),
+    path('kashta/home', kashta.home, name='kashta-home'),
+    path('bladalnoor/home', bladalnoor.home, name='bladalnoor-home'),
+    path('batinjan/home', batinjan.home, name='batinjan-home'),
 ]
---
+++ P001/views/batinjan.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
+from django.utils import translation
+from django.shortcuts import redirect
+from django.conf import settings
+from django.utils import http
+from django.http import HttpResponseRedirect
+
+@login_required
+def home(request):
+
+    context = {
+    }
+
+    return render(request, 'P001_batinjan/home.html', context=context)
